710495cbe6710911812de3bf34d88cd03c120cab
carlosismaelad <carlosismaelad@outlook.com>
2023-09-27 13:46:03 -0300

    Primeiro exercício/projeto com React e Vite

---
+++ 03-primeiro-projeto-react/src/App.jsx
@@ -0,0 +1,31 @@
+export default function App(){
+  return (
+    <div>
+      <img src="./react.png" alt="Logo do React" />
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuário web e nativas.</p>
+      <div>
+        <button>Aprenda React</button>
+        <button>Referência da API</button>
+      </div>
+      <hr />
+      <h2>Crie interfaces de usuário de componentes</h2>
+      <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes</p>
+      <hr />
+      <h2>Escreva componentes com código e marcação</h2>
+      <p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo React.</p>
+      <hr />
+      <h2>Próximos passos:</h2>
+      <ul>       
+        <li>Praticar com exercícios</li>
+        <li>Aprender a usar dados dinâmicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar os componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </div>
+  )
+}
+
