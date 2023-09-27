aa99e5c61d844517a37d25dec26b4d834cfd46df
carlosismaelad <carlosismaelad@outlook.com>
2023-09-27 14:45:47 -0300

    Organização pasta components, novos componentes e modos de export

---
+++ 04-dados-dinamicos-jsx/src/components/Status.jsx
@@ -0,0 +1,7 @@
+function Status() {
+    const status = true
+
+    return <h2>Vamos estudar? {status ? "Of course!" : "Oh, no! :("}</h2>
+}
+
+export default Status
---
+++ 04-dados-dinamicos-jsx/src/components/Subtitle.jsx
@@ -0,0 +1,7 @@
+function sum(a,b){
+    return a + b
+}
+
+export const Subtitle = () => <h2>Like 1 + 1 is {sum(1,1)}</h2>
+    
+
