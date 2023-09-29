239fb7a8196b9ebcc03da5271a8666d77f467fca
carlosismaelad <carlosismaelad@outlook.com>
2023-09-28 22:56:56 -0300

    Card -> props - entendendo e adicionando props

--- 05-exer-criando-card/src/App.jsx
+++ 05-exer-criando-card/src/App.jsx
@@ -1,11 +1,15 @@
 import Card from "./components/Card"
+import swPosterImg from "./assets/starwars.jpg"
+import esbPosterImg from "./assets/empire-strikes-back.jpg"
+import rotjPosterImg from "./assets/return-of-the-jedi.jpg"
 
 function App() {
   return (
     <>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card title="Pôster: Star Wars (1977)" posterImg={swPosterImg}/>
+      <Card title="Pôster: Empire Strikes Back (1980)" posterImg={esbPosterImg}/>
+      <Card title="Pôster: Return of the Jedi (1983)" posterImg={rotjPosterImg}/>
+      
     </>
   )
 }
--- 05-exer-criando-card/src/components/Card/index.jsx
+++ 05-exer-criando-card/src/components/Card/index.jsx
@@ -1,16 +1,23 @@
-import posterImg from "../../assets/starwars.jpg"
+import PropTypes from "prop-types"
 import Button from "../Button"
 import styles from "./style.module.css"
 
-export default function Card(){
+export default function Card({ title, posterImg }){    
+
     return (
         <div className={styles.container}>
-            <img className={styles.poster} src={posterImg} alt="Star Wars Poster" />
+            <img className={styles.poster} src={posterImg} alt={title} />
             <div>
-                <h2>Pôster do filme Star Wars</h2>
+                <h2>{title}</h2>
                 <p>Um pôster decorativo épico do filme Star Wars, com moldura em MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar onde você decidir pendurar. Não peca a chance de adicionar essa linda memória ao seu acervo!</p>
                 <Button/>
             </div>
         </div>
     )
 }
+
+Card.propTypes = {
+    title: PropTypes.string.isRequired,
+    posterImg: PropTypes.string.isRequired
+}
+
